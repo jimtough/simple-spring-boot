b1751e6bfbd4ef995b91a0d755e07df9cc38aba3
adultmalehuman <adultmalehuman@gmail.com>
2019-09-01 22:10:28 -0300

    Added some silly metrics with dimensions as an example

--- src/main/java/com/adultmalehuman/ssb/GlobalConstants.java
+++ src/main/java/com/adultmalehuman/ssb/GlobalConstants.java
@@ -10,4 +10,10 @@ public final class GlobalConstants {
 
 	public static final String METRIC_PAGEHITS_TOTAL_COUNT = "amh.pagehits.total.count";
 
+	public static final String METRIC_RANDOM_BOOLEAN = "amh.random.boolean";
+	public static final String METRIC_RANDOM_INTEGER = "amh.random.integer";
+	public static final String METRIC_RANDOM_DOUBLE = "amh.random.double";
+
+	public static final String DIMENSION_PHOENETIC = "phoenetic";
+	public static final String DIMENSION_ZABRABOOF = "zabraboof";
 }
--- src/main/java/com/adultmalehuman/ssb/mvc/MyRestController.java
+++ src/main/java/com/adultmalehuman/ssb/mvc/MyRestController.java
@@ -1,11 +1,17 @@
 package com.adultmalehuman.ssb.mvc;
 
 import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import com.adultmalehuman.ssb.GlobalConstants;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,17 +37,76 @@ public class MyRestController {
 	@Value("${app.version}")
 	private String appVersionString;
 
+	private final Random random;
 	private final AtomicInteger requestCountTotal;
+	private final AtomicBoolean randomBooleanValueA;
+	private final AtomicBoolean randomBooleanValueB;
+	private final AtomicBoolean randomBooleanValueC;
+	private final AtomicBoolean randomBooleanValueD;
+	private final AtomicReference<Double> randomDoubleReference;
+
+	private static double atomicBooleanToDouble(final AtomicBoolean ab) {
+		boolean b = Objects.requireNonNull(ab).get();
+		return b ? 1 : 0;
+	}
 
 	MyRestController(MeterRegistry meterRegistry) {
+		this.random = new Random();
 		Objects.requireNonNull(meterRegistry, "meterRegistry cannot be null");
-		this.requestCountTotal = meterRegistry.gauge(GlobalConstants.METRIC_PAGEHITS_TOTAL_COUNT, new AtomicInteger(0));
+		this.requestCountTotal = meterRegistry.gauge(
+				GlobalConstants.METRIC_PAGEHITS_TOTAL_COUNT,
+				new AtomicInteger(0));
+		this.randomBooleanValueA = meterRegistry.gauge(
+				GlobalConstants.METRIC_RANDOM_BOOLEAN,
+				Arrays.asList(
+					Tag.of(GlobalConstants.DIMENSION_PHOENETIC, "alpha"),
+					Tag.of(GlobalConstants.DIMENSION_ZABRABOOF, "foo")
+				),
+				new AtomicBoolean(false),
+				MyRestController::atomicBooleanToDouble);
+		this.randomBooleanValueB = meterRegistry.gauge(
+				GlobalConstants.METRIC_RANDOM_BOOLEAN,
+				Arrays.asList(
+					Tag.of(GlobalConstants.DIMENSION_PHOENETIC, "alpha"),
+					Tag.of(GlobalConstants.DIMENSION_ZABRABOOF, "bar")
+				),
+				new AtomicBoolean(false),
+				MyRestController::atomicBooleanToDouble);
+		this.randomBooleanValueC = meterRegistry.gauge(
+				GlobalConstants.METRIC_RANDOM_BOOLEAN,
+				Arrays.asList(
+					Tag.of(GlobalConstants.DIMENSION_PHOENETIC, "baker"),
+					Tag.of(GlobalConstants.DIMENSION_ZABRABOOF, "foo")
+				),
+				new AtomicBoolean(false),
+				MyRestController::atomicBooleanToDouble);
+		this.randomBooleanValueD = meterRegistry.gauge(
+				GlobalConstants.METRIC_RANDOM_BOOLEAN,
+				Arrays.asList(
+					Tag.of(GlobalConstants.DIMENSION_PHOENETIC, "baker"),
+					Tag.of(GlobalConstants.DIMENSION_ZABRABOOF, "bar")
+				),
+				new AtomicBoolean(false),
+				MyRestController::atomicBooleanToDouble);
+		this.randomDoubleReference = meterRegistry.gauge(
+				GlobalConstants.METRIC_RANDOM_DOUBLE,
+				Collections.singletonList(
+					Tag.of(GlobalConstants.DIMENSION_PHOENETIC, "charlie")
+				),
+				new AtomicReference<>((double) 0),
+				AtomicReference::get);
 	}
 
 	static final String CANNED_REPLY_STRING = "Greetings from my Spring Boot app!";
 
 	@RequestMapping("/")
 	public String index() {
+		LOGGER.debug("Request received");
+		randomBooleanValueA.set(random.nextBoolean());
+		randomBooleanValueB.set(random.nextBoolean());
+		randomBooleanValueC.set(random.nextBoolean());
+		randomBooleanValueD.set(random.nextBoolean());
+		randomDoubleReference.set(random.nextDouble());
 		final int pageHits = requestCountTotal.incrementAndGet();
 		return String.format(RESPONSE_HTML_TEMPLATE, pageHits, ZonedDateTime.now(), appVersionString);
 	}
