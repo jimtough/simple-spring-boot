0953d0d877eaf1ee156cf0a5dd9d62da6a7a3955
adultmalehuman <adultmalehuman@gmail.com>
2019-10-20 15:39:42 -0300

    Disable many auto-enabled metrics

--- src/main/java/com/adultmalehuman/ssb/Application.java
+++ src/main/java/com/adultmalehuman/ssb/Application.java
@@ -4,11 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.JvmMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.LogbackMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.servlet.WebMvcMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = {
+		JvmMetricsAutoConfiguration.class,
+		LogbackMetricsAutoConfiguration.class,
+		SystemMetricsAutoConfiguration.class,
+		TomcatMetricsAutoConfiguration.class,
+		WebMvcMetricsAutoConfiguration.class,
+		SimpleMetricsExportAutoConfiguration.class,
+		SystemMetricsAutoConfiguration.class })
 public class Application {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
